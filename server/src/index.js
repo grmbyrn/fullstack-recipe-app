40ce98824a503112566692741735515e4e314d38
grmbyrn <graemebyrne_89@hotmail.com>
2023-03-09 00:26:13 +0100

    remove mongodb string and set up recipes model and routes

--- server/src/index.js
+++ server/src/index.js
@@ -4,19 +4,16 @@ import mongoose from 'mongoose'
 import { config } from 'dotenv'
 
 import {userRouter} from './routes/users.js'
+import {recipesRouter} from './routes/recipes.js'
 
 const app = express()
 config()
 
 mongoose.connect(process.env.DB_STRING, { useNewUrlParser: true });
 
-console.log(process.env.DB_STRING)
-
 app.use(express.json())
 app.use(cors())
 
 app.use('/auth', userRouter)
-
-mongoose.connect('mongodb+srv://graemebyrne:carrermozart20@cluster0.ei6dhms.mongodb.net/recipes?retryWrites=true&w=majority')
 
 app.listen(3002, () => console.log('Server Started'))
