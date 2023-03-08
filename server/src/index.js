21889f6270c73f2f7cf535e93fd9443463359a2c
grmbyrn <graemebyrne_89@hotmail.com>
2023-03-09 00:15:40 +0100

    install dotenv to hide mongodb string

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,22 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import { config } from 'dotenv'
+
+import {userRouter} from './routes/users.js'
 
 const app = express()
+config()
+
+mongoose.connect(process.env.DB_STRING, { useNewUrlParser: true });
+
+console.log(process.env.DB_STRING)
 
 app.use(express.json())
-
 app.use(cors())
 
-app.listen(3001, () => console.log('Server Started'))
+app.use('/auth', userRouter)
+
+mongoose.connect('mongodb+srv://graemebyrne:carrermozart20@cluster0.ei6dhms.mongodb.net/recipes?retryWrites=true&w=majority')
+
+app.listen(3002, () => console.log('Server Started'))
